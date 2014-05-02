3d12d7e6fb3f55b44a1b1e899507717f79a93611
jonathanoh0689 <jonathanoh0689@gmail.com>
2014-05-02 09:51:53 -0700

    Sync 5-2-14

--- java.js
+++ java.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
+var xMark = '<span class="delete"></span>'
 
-$('.items-field').keypress(function(x) {
-	var list = $(this).text();
-	$(this).append(list);
+$('.items-field').keypress(function(event) {
+	var item = $(this).val();
+	var list = $('.list');
+	if(event.keyCode == 13) {
+		list.append('<li>' + xMark + item + '</li>');
+	}
+	
 });
 
 
 });
+
