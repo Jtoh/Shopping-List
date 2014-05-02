3127ea0ac8cd2d9ec15e6e14ac04fa2d37aec280
jonathanoh0689 <jonathanoh0689@gmail.com>
2014-05-02 14:41:37 -0700

    update 5-2-14

--- java.js
+++ java.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
 
-$('.items-field').keypress(function(x) {
-	var list = $(this).text();
-	$(this).append(list);
+$('.items-field').keypress(function(event) {
+	var list = $('.list-items');
+	var item = $(this).val();
+	var xMark = '<span class="delete"></span>';
+	if(event.keyCode == 13) {
+		$(list).append('<li>' + item + xMark + '</li>');
+		$(this).val('');
+	}
 });
 
 
